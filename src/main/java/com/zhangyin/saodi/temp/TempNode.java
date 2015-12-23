70ebd9f994aa7576d793c2ac46122d0e656fc2ad
张银 <459402930@qq.com>
2015-12-23 10:57:29 +0800

    结构还是有点混乱

---
+++ src/main/java/com/zhangyin/saodi/temp/TempNode.java
@@ -0,0 +1,19 @@
+package com.zhangyin.saodi.temp;
+
+import com.zhangyin.saodi.base.Direction;
+import com.zhangyin.saodi.base.Node;
+
+public class TempNode {
+	
+	public Node n;
+	
+	public Direction d;
+
+	public TempNode(Node n, Direction d) {
+		super();
+		this.n = n;
+		this.d = d;
+	}
+
+	
+}
